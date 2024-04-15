b1dd6c679e2590242f81dd27f4fc4da31fe14be6
thienphuc0108 <phucsky38@gmail.com>
2024-04-16 03:09:35 +0700

    user.php update key

--- app/Models/User.php
+++ app/Models/User.php
@@ -48,4 +48,8 @@ class User extends Authenticatable
     {
         return $this->belongsTo(Role::class);
     }
+    public function ctVips()
+    {
+        return $this->hasMany(CtVip::class, 'user_id');
+    }
 }
